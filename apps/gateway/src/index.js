864db068bef311e821cdfbe0daea31b0942bd07e
kinghappy <kalikpance@gmail.com>
2026-03-23 11:19:52 +0700

    add readme

--- apps/gateway/src/index.js
+++ apps/gateway/src/index.js
@@ -1,10 +1,11 @@
 import { createLogger } from '@acme/observability'
 import config from './config/env.js'
+import { createApp } from './app.js'
 
 const { port, authUrl, upstreamTimeoutMs, nodeEnv, version, serviceName } = config
 
 const logger = createLogger({ serviceName, nodeEnv, version })
 
-const app = createLogger({config, logger})
+const app = createApp({ config, logger })
 
 app.listen(port, () => logger.info("service name: ", serviceName))
